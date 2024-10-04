bb639dd28f6f9b6ef011952350a7027313199e38
Sachin9168 <116281462+Sachin9168@users.noreply.github.com>
2024-10-04 09:32:57 +0530

    Update developer.py

--- developer.py
+++ developer.py
@@ -5,7 +5,7 @@ class Developer:
     def __init__(self, root):
         self.root = root
         self.root.geometry("1530x790+0+0")
-        self.root.title("AI Based Face Attendance System")
+        self.root.title("Advance Facial Recognition Attendance System")
 
         title_lbl = Label(self.root, text="DEVELOPER", font=("times new roman", 35, "bold"), bg="green",
                           fg="white")
