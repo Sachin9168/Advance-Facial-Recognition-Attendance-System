026e32d77b442a3ec41009060066a65529b94a8d
Sachin9168 <116281462+Sachin9168@users.noreply.github.com>
2024-10-04 09:25:36 +0530

    Update attendence.py

--- attendence.py
+++ attendence.py
@@ -12,7 +12,7 @@ class Attendence:
     def __init__(self, root):
         self.root = root
         self.root.geometry("1530x790+0+0")
-        self.root.title("AI Based Face Attendance System")
+        self.root.title("Advance Facial Recognition Attendance System")
 
 
         # _____________________________variable_________
